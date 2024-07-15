f33e2253d8a38de442bf2f342eb5758f89f63643
joao victor <joaovprudencio13@gmail.com>
2024-07-15 13:46:39 -0300

    controller finzalido

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -15,24 +15,35 @@ const BuscarUsuario = async (req, res) => {
     const busca = await service.BuscarUsuario(id);
     res.status(200).json(usuario);
   } catch (error) {
-    res.status(404).json({error: error.messege})
+    res.status(404).json({ error: error.messege });
   }
 };
 
-const ExcluirUsuario = async (req , res) => {
-   const {id} = req.params;
-   try {
+const ExcluirUsuario = async (req, res) => {
+  const { id } = req.params;
+  try {
     const Excluir = await service.DeletarUsuario(id);
     res.status(204).send();
-   } catch (error) {
-    res.status(400).json({error: error.messege});
-   }
+  } catch (error) {
+    res.status(400).json({ error: error.messege });
+  }
 };
 
-const ListarUsuarios = async (req , res) => {
-try {
-    const listar = service.Listar();
-} catch(error) {
-    
-}
-}
+const ListarUsuarios = async (req, res) => {
+  try {
+    const usuarios = service.Listar();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(400).json({ error: error.messege });
+  }
+};
+
+const atualizar = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const UsuarioAtualizado = await service.AtualizarUsuario(id, req.body);
+    req.status(200).json(UsuarioAtualizado);
+  } catch (error) {
+    res.status(400).json({ error: error.messege });
+  }
+};
